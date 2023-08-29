4b25f639f5144f63d84531c0272493cca838d9a6
igorkukric <you@example.com>
2023-08-29 17:19:35 +0200

    Generate Html to display meal data

--- script.js
+++ script.js
@@ -22,15 +22,32 @@ function searchMeal() {
   // Fetch meal data using api with user input
   fetch(apiUrl + userInput)
     .then((response) => response.json())
-    .then((data)=> {
-        const meal = data.meals[0]
-        // Handle where no meal found
-        if(!meal) {
-            resultContainer = innerHTML `<h3>No Meal Found, Please Try Again</h3>`
-            return
-        }
-        const ingredients = getIngredients(meal)
-        // Generate Html to display meal data
+    .then((data) => {
+      const meal = data.meals[0];
+      // Handle where no meal found
+      if (!meal) {
+        resultContainer = innerHTML`<h3>No Meal Found, Please Try Again</h3>`;
+        return;
+      }
+      const ingredients = getIngredients(meal);
+      // Generate Html to display meal data
+      const recipeHtml = `
+            <div class="details">
+                <h2>${meal.strMeal}</h2>
+                <h4>${meal.strArea}</h4>
+            </div>
+            <img src=${meal.strMealThumb} alt=${meal.strMeal} />
+            <div id="ingre-container">
+                <h3>Ingredients:</h3>
+                <ul>${ingredients}</ul>
+            </div>
+            <div id="recipe">
+                <button id="hide-recipe">X</button>
+                <pre id="instructions">${meal.strInstructions}</pre>
+            </div>
+            <button id="show-recipe">View Recipe</button>     
         
-    })
+        `;
+      resultContainer.innerHTML = recipeHtml;
+    });
 }
