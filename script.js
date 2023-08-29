e120741059e7139144f1061a99bbfd93c26c6fb1
igorkukric <you@example.com>
2023-08-29 16:49:51 +0200

    setting constsances

---
+++ script.js
@@ -0,0 +1,5 @@
+const resultContainer = document.getElementById("result")
+const searchBtn = document.getElementById("search-button")
+const searchInput = document.getElementById("search-input")
+const searchContainer = document.querySelector(".search-box")
+
