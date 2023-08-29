5f4ab1a8c7ab06c727457ff9fef0c238f3c36c40
igorkukric <you@example.com>
2023-08-29 16:57:58 +0200

    add listeners and apiurl

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const searchBtn = document.getElementById("search-button")
 const searchInput = document.getElementById("search-input")
 const searchContainer = document.querySelector(".search-box")
 
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+
+searchBtn.addEventListener("click", searchMeal)
+searchInput.addEventListener("keydown", function (e){
+    if(e.keyCode === 13) {
+        e.preventDefault()
+        searchMeal()
+    }
+})
