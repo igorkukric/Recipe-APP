6f075480966a5c19ad902d100e789d0e762672a6
igorkukric <you@example.com>
2023-08-29 17:08:04 +0200

    Handle meal function Fetch meal data using api with user input Handle where no meal found

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const resultContainer = document.getElementById("result")
-const searchBtn = document.getElementById("search-button")
-const searchInput = document.getElementById("search-input")
-const searchContainer = document.querySelector(".search-box")
+const resultContainer = document.getElementById("result");
+const searchBtn = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const searchContainer = document.querySelector(".search-box");
 
-const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-searchBtn.addEventListener("click", searchMeal)
-searchInput.addEventListener("keydown", function (e){
-    if(e.keyCode === 13) {
-        e.preventDefault()
-        searchMeal()
-    }
-})
+searchBtn.addEventListener("click", searchMeal);
+searchInput.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    searchMeal();
+  }
+});
+// Handle meal function
+function searchMeal() {
+  const userInput = searchInput.ariaValueMax.trim();
+  if (!userInput) {
+    resultContainer.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+    return;
+  }
+  // Fetch meal data using api with user input
+  fetch(apiUrl + userInput)
+    .then((response) => response.json())
+    .then((data)=> {
+        const meal = data.meals[0]
+        // Handle where no meal found
+        if(!meal) {
+            resultContainer = innerHTML `<h3>No Meal Found, Please Try Again</h3>`
+            return
+        }
+        const ingredients = getIngredients(meal)
+        // Generate Html to display meal data
+        
+    })
+}
